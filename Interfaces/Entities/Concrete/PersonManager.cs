3d9cc97b7ea091fe61c53a3a1270d814b694b767
ArdaSunar <arda.sunar@windowslive.com>
2024-03-16 00:20:20 +0300

    Interfaces Update

---
+++ Interfaces/Entities/Concrete/PersonManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces.Entities.Abstract;
+using Interfaces.Entities.Concrete;
+
+namespace Interfaces.Entities.Concrete
+{
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Interfaces.Entities.Abstract;
+using Interfaces.Entities.Concrete;
+
+//InterfacesIntro();
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add(new OracleCustomerDal());
+Console.ReadLine();
+
+static void InterfacesIntro()
+{
+    PersonManager manager = new PersonManager();
+    manager.Add(new Customer { Id = 1, FirstName = "Arda", LastName = "Sunar", Adress = "Samsun" });
+
+    Student student = new Student
+    {
+        Id = 2,
+        FirstName = "Abuzer",
+        LastName = "Sunar",
+        Department = "Computer Science"
+    };
+
+    manager.Add(student);
+}
